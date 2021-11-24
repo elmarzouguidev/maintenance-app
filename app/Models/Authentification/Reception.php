3bb0315f67cdab5a9dcf23bd5120d363bb63362c
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-24 17:17:33 +0100

    add Reception Model and migration , Factory Class

---
+++ app/Models/Authentification/Reception.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Authentification;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reception extends Model
+{
+    use HasFactory;
+}
---
+++ database/factories/Authentification/ReceptionFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Authentification;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReceptionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
