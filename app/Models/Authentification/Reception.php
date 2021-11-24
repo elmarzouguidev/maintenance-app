1229b6d642e080b034b4e8101c532457b2d9ef6b
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-24 21:18:32 +0100

    change Reception model to be Authenticable

--- app/Models/Authentification/Reception.php
+++ app/Models/Authentification/Reception.php
@@ -3,9 +3,47 @@
 namespace App\Models\Authentification;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class Reception extends Model
+class Reception extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'telephone',
+        'email',
+        'password',
+        'active'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'active'=>'boolean',
+    ];
+
 }
