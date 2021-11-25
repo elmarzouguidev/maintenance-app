4d8f071e1ee2d4ec32747f537149b4db26a3ea9e
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-25 14:25:17 +0100

    change welcom view text by adding text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,7 +125,8 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                         <br>
-                        Elmarzougui Abdelghafour
+                        Elmarzougui Abdelghafour <a href="https://elmarzougui.net" target="_blank">website</a>
+
                     </div>
                 </div>
             </div>
