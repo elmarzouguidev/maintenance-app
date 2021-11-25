8da7abb95b1856440e1866843e47dee7521a4722
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-25 16:56:09 +0100

    add Repositories Folder (Pattern), add Admin Reposotory with interface and repository Class

---
+++ app/Repositories/Admin/AdminInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Repositories\Admin;
+
+
+interface AdminInterface
+{
+
+
+    public function getAdmins();
+
+    public function getAdmin();
+
+}
---
+++ app/Repositories/Admin/AdminRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Repositories\Admin;
+
+
+class AdminRepository implements AdminInterface
+{
+
+    /**
+     * @return mixed
+     */
+    public function getAdmins()
+    {
+        // TODO: Implement getAdmins() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdmin()
+    {
+        // TODO: Implement getAdmin() method.
+    }
+}
