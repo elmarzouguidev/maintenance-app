2abcd402c3b2ee103c93f83f9486a15aa621a3d1
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-26 23:14:24 +0100

    remove rand function from testAdminRepository

--- tests/Feature/Repositories/Admin/AdminRepositoryTest.php
+++ tests/Feature/Repositories/Admin/AdminRepositoryTest.php
@@ -42,7 +42,7 @@ class AdminRepositoryTest extends TestCase
     {
         Admin::factory(20)->superAdmin()->create();
 
-        $admin = app(AdminInterface::class)->getAdmin(rand(1, 100));
+        $admin = app(AdminInterface::class)->getAdmin(rand(2));
 
         $admin->delete();
 
