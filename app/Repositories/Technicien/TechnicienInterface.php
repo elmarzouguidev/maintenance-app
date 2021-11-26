6bff1a83daeec63a2fe93e25652b185b9fe6fca7
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-26 23:12:00 +0100

    add Technicien Repository with Interface to the App\Repositories Folder Ã

---
+++ app/Repositories/Technicien/TechnicienInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Repositories\Technicien;
+
+
+interface TechnicienInterface
+{
+
+    public function getTechniciens();
+
+    public function getTechnicien();
+
+    public function addTechnicien(array $data);
+}
