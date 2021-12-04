6b6e8dc5e0943be3d5bc5fe909ac46efb54b7fc2
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-12-04 21:51:50 +0100

    add test for check that can give a permission to admin

--- tests/Feature/Roles/AdminRolesTest.php
+++ tests/Feature/Roles/AdminRolesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Roles;
 
 use App\Models\Authentification\Admin;
+use Elmarzougui\Roles\Helpers\Permissions;
 use Elmarzougui\Roles\Helpers\Roles;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,12 +14,23 @@ class AdminRolesTest extends TestCase
 
     public function test_give_role_to_admin()
     {
-        Roles::new()->firstOrCreate(['name' => 'writer'], ['name' => 'writer', 'guard_name' => 'admin']);
+        $role =  Roles::new()->firstOrCreate(['name' => 'writer'], ['name' => 'writer', 'guard_name' => 'admin']);
 
         $admin = Admin::factory()->create();
 
         $admin->assignRole('writer');
 
-        $this->assertDatabaseHas('model_has_roles', ['model_id' => $admin->id, 'role_id' => 1]);
+        $this->assertDatabaseHas('model_has_roles', ['model_id' => $admin->id, 'role_id' => $role->id]);
+    }
+
+    public function test_give_permission_to_admin()
+    {
+        $admin = Admin::factory()->create();
+
+        $permission = Permissions::new()->firstOrCreate(['name' => 'add tickets'], ['name' => 'add tickets', 'guard_name' => 'admin']);
+
+        $admin->givePermissionTo('add tickets');
+
+        $this->assertDatabaseHas('model_has_permissions', ['model_id' => $admin->id, 'permission_id' => $permission->id]);
     }
 }
