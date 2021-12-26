d4739e7dcbf32f883377c9ee9b4b210a61060cd9
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-12-26 12:44:40 +0100

    add external_id to contacts table

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Traits\UuidGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
     use HasFactory;
+    use UuidGenerator;
 }
