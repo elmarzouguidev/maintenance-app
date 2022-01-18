61f32f97dd0192f81db5ec9298b2d893c0b317d1
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-01-18 09:41:38 +0100

    work on Reception interface fix Diagnose  v3

--- resources/views/theme/layouts/_parts/__header.blade.php
+++ resources/views/theme/layouts/_parts/__header.blade.php
@@ -3,7 +3,7 @@
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="{{route('admin:home')}}" class="logo logo-dark">
+                {{--<a href="{{route('admin:home')}}" class="logo logo-dark">
                             <span class="logo-sm">
                                 <img src="{{asset('assets/images/logo.svg')}}" alt="" height="22">
                             </span>
@@ -19,7 +19,7 @@
                     <span class="logo-lg">
                                 <img src="{{asset('assets/images/logo-light.png')}}" alt="" height="19">
                             </span>
-                </a>
+                </a>--}}
             </div>
 
             <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
