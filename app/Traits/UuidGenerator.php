bdbcbd2bcfa2b256732bede4ead3ce61a2f44d50
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-01-21 19:07:14 +0100

    change external_id by uuid in UuidGenerator Trait

--- app/Traits/UuidGenerator.php
+++ app/Traits/UuidGenerator.php
@@ -9,16 +9,13 @@ use Illuminate\Support\Facades\Schema;
 trait UuidGenerator
 {
 
-    public static function bootUuidGenerator() : void
+    public static function bootUuidGenerator(): void
     {
 
         static::creating(function ($model) {
-            if (Schema::hasColumn($model->getTable(), 'external_id')) {
-                $model->external_id = Str::uuid();
-            } else {
-                // dd('Im here inside call migrate');
-               // Artisan::call('migrate', ['--path' => '/database/migrations/2021_12_17_001853_add_external_id_to_all_table.php']);
-               // $model->external_id = Str::uuid();
+
+            if (Schema::hasColumn($model->getTable(), 'uuid')) {
+                $model->uuid = Str::uuid();
             }
         });
     }
