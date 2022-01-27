9516e4a48d524bfb14669c600ea98daee554bdcc
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-01-27 19:03:34 +0100

    Add PDFPublicController to make PDF files visible for public ( Clients ,... )

--- app/Http/Controllers/Web/PDFPublicController.php
+++ app/Http/Controllers/Web/PDFPublicController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Finance\Invoice;
 use Illuminate\Http\Request;
 
 class PDFPublicController extends Controller
 {
-    //
+
+
+    public function showInvoice(Invoice $invoice)
+    {
+
+        $invoice->load('articles', 'company', 'client');
+
+        $companyLogo = $invoice->company->logo;
+
+        $pdf = \PDF::loadView('theme.invoices_template.template1.index', compact('invoice', 'companyLogo'));
+
+        return $pdf->stream($invoice->date_invoice . "-[ {$invoice->client->entreprise} ]-" . 'facture.pdf');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Site\CategoryController;
 use App\Http\Controllers\Site\ClientController;
 use App\Http\Controllers\Site\SiteController;
+use App\Http\Controllers\Web\PDFPublicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'views'], function () {
+
+    Route::group(['prefix' => 'invoices'], function () {
+        Route::get('/invoice/{invoice}', PDFPublicController::class, 'showInvoice')->name('public.show.invoice');
+    });
+});
