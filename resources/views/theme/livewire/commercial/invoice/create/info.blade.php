f93b0042fd94e2e740acec05a856bd110f5c5b4f
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-01-30 18:57:39 +0100

    Add key to livewire loop v2

--- resources/views/theme/livewire/commercial/invoice/create/info.blade.php
+++ resources/views/theme/livewire/commercial/invoice/create/info.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-lg-4">
-        <div class="mb-4">
+        <div class="mb-4" wire:key="companies">
             <label class="form-label">Société (Vous) *</label>
             <select wire:model="selectCompany" name="company"
                 class="form-control select2 @error('company') is-invalid @enderror" required>
@@ -21,7 +21,7 @@
 
         </div>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-4" wire:key="clients">
         <div class="mb-4">
             <label class="form-label">Client *</label>
             <select wire:model.lazy="selectClient" name="client"
@@ -42,7 +42,7 @@
 
         </div>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-4" wire:key="tickets">
         <div class="mb-4">
             <label class="form-label">Ticket *</label>
             <select wire:model.defer="selectTicket" name="ticket"
@@ -65,7 +65,7 @@
     </div>
     <div class="docs-options">
         <label class="form-label">Numéro de facture</label>
-        <div class="input-group mb-4">
+        <div class="input-group mb-4" wire:key="invoice">
 
             <span class="input-group-text" id="invoice_prefix">
                 {{ $invoicePrefix }}
