c093e867efe26f992c00ed40cf23ea0959faea68
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-01-30 18:54:09 +0100

    Add key to livewire loop

--- resources/views/theme/livewire/commercial/invoice/create/info.blade.php
+++ resources/views/theme/livewire/commercial/invoice/create/info.blade.php
@@ -1,84 +1,84 @@
-<div>
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="mb-4">
-                <label class="form-label">Société (Vous) *</label>
-                <select wire:model="selectCompany" name="company"
-                    class="form-control select2 @error('company') is-invalid @enderror" required>
-                    <option value="">Select</option>
+<div class="row">
+    <div class="col-lg-4">
+        <div class="mb-4">
+            <label class="form-label">Société (Vous) *</label>
+            <select wire:model="selectCompany" name="company"
+                class="form-control select2 @error('company') is-invalid @enderror" required>
+                <option value="">Select</option>
 
-                    @foreach ($companies as $company)
-                        <option value="{{ $company->id }}">
-                            {{ $company->name }}
-                        </option>
-                    @endforeach
+                @foreach ($companies as $company)
+                    <option value="{{ $company->id }}" wire:key="{{ $loop->index }}">
+                        {{ $company->name }}
+                    </option>
+                @endforeach
 
-                </select>
-                @error('company')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="mb-4">
-                <label class="form-label">Client *</label>
-                <select wire:model.lazy="selectClient" name="client"
-                    class="form-control select2 @error('client') is-invalid @enderror" required>
-                    <option value="">Select</option>
-                    @foreach ($clients as $client)
-                        <option value="{{ $client->id }}">{{ $client->entreprise }}</option>
-                    @endforeach
-
-                </select>
-                {{-- <h1>{{ $selectClient }}</h1> --}}
-                @error('client')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="mb-4">
-                <label class="form-label">Ticket *</label>
-                <select wire:model.defer="selectTicket" name="ticket"
-                    class="form-control select2 @error('ticket') is-invalid @enderror">
-                    <option value="">Select</option>
-
-                    @foreach ($tickets as $ticket)
-                        <option value="{{ $ticket->id }}">{{ $ticket->article }}</option>
-                    @endforeach
-
-                </select>
-                @error('ticket')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-
-            </div>
-        </div>
-        <div class="docs-options">
-            <label class="form-label">Numéro de facture</label>
-            <div class="input-group mb-4">
-
-                <span class="input-group-text" id="invoice_prefix">
-                    {{ $invoicePrefix }}
+            </select>
+            @error('company')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
                 </span>
+            @enderror
 
-                <input type="text" class="form-control @error('invoice_code') is-invalid @enderror" name="invoice_code"
-                    value="" wire:model.defer="invoiceCode" aria-describedby="invoice_prefix" readonly>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="mb-4">
+            <label class="form-label">Client *</label>
+            <select wire:model.lazy="selectClient" name="client"
+                class="form-control select2 @error('client') is-invalid @enderror" required>
+                <option value="">Select</option>
+                @foreach ($clients as $client)
+                    <option value="{{ $client->id }}" wire:key="{{ $loop->index }}">{{ $client->entreprise }}
+                    </option>
+                @endforeach
 
-                @error('invoice_code')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+            </select>
+            {{-- <h1>{{ $selectClient }}</h1> --}}
+            @error('client')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="mb-4">
+            <label class="form-label">Ticket *</label>
+            <select wire:model.defer="selectTicket" name="ticket"
+                class="form-control select2 @error('ticket') is-invalid @enderror">
+                <option value="">Select</option>
+
+                @foreach ($tickets as $ticket)
+                    <option value="{{ $ticket->id }}" wire:key="{{ $loop->index }}">{{ $ticket->article }}
+                    </option>
+                @endforeach
+
+            </select>
+            @error('ticket')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+        </div>
+    </div>
+    <div class="docs-options">
+        <label class="form-label">Numéro de facture</label>
+        <div class="input-group mb-4">
+
+            <span class="input-group-text" id="invoice_prefix">
+                {{ $invoicePrefix }}
+            </span>
+
+            <input type="text" class="form-control @error('invoice_code') is-invalid @enderror" name="invoice_code"
+                value="" wire:model.defer="invoiceCode" aria-describedby="invoice_prefix" readonly>
+
+            @error('invoice_code')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
 </div>
