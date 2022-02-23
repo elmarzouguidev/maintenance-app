ab7ef204564f445e2c82a5fb2b5e757c6bb4dea4
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-02-23 02:11:24 +0100

    Fix Estimate Email template v2

--- resources/views/theme/Emails/Commercial/Estimate/SendEstimateMail.blade.php
+++ resources/views/theme/Emails/Commercial/Estimate/SendEstimateMail.blade.php
@@ -1,18 +1,14 @@
 <div class="text" style="padding: 0 2.5em;">
-
-    <h3>DEVIS N° : {{$data->code}}</h3>
-
     <p>Cher client,</p><br>
-
     <p> Veuillez trouver en pièce jointe le devis {{$data->code}} .</p>
-
-    <p> Détails de devis :</p><br>
-    <p><strong>N° de devis:</strong> {{$data->code}}</p><br>
-    <p><strong>Date:</strong> {{$data->estimate_date->format('d-m-Y')}}</p><br>
-    <p><strong> Montant: </strong> MAD {{ $data->formated_price_total }}</p><br>
-
-    <p>Si vous n'avez toujours pas communiqué votre ICE, merci de nous l'envoyer en
+    <p> Détails de devis :</p>
+    <p><strong>N° de devis:</strong> {{$data->code}}</p>
+    <p><strong>Date:</strong> {{$data->estimate_date->format('d-m-Y')}}</p>
+    <p><strong> Montant: </strong> MAD {{ $data->formated_price_total }}</p>
+    <p>
+        Si vous n'avez toujours pas communiqué votre ICE, merci de nous l'envoyer en
         réponse à ce mail ou à l'adresse contact@casamaintenance.ma.
 
-        Notre service reste à votre disposition pour toute demande.</p>
+        Notre service reste à votre disposition pour toute demande.
+    </p>
 </div>
