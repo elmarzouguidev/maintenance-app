1baec3f5fae2bb3cb07db7a23311b3b5a2d4f785
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-02-24 01:09:47 +0100

    Hide price from estimate email template

--- resources/views/theme/Emails/Commercial/Estimate/SendEstimateMail.blade.php
+++ resources/views/theme/Emails/Commercial/Estimate/SendEstimateMail.blade.php
@@ -5,7 +5,7 @@
     <ul>
         <li><strong>N° de devis:</strong> {{$data->code}}</li>
         <li><strong>Date:</strong> {{$data->estimate_date->format('d-m-Y')}}</li>
-        <li><strong>Montant: </strong> MAD {{ $data->formated_price_total }}</li>
+        {{--<li><strong>Montant: </strong> MAD {{ $data->formated_price_total }}</li>--}}
     </ul>
     <p>
         {{--Si vous n'avez toujours pas communiqué votre ICE, merci de nous l'envoyer en
@@ -13,6 +13,5 @@
 
         Notre service reste à votre disposition .
         pour toute demande veuillez nous contacter sur l'email suivant {{$data->company->email}}.
-
     </p>
 </div>
