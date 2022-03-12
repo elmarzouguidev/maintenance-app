94b02a0f06000df19e835303dcae9b44ea02650a
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-12 16:04:04 +0100

    fix permission for client

--- resources/views/theme/pages/Client/__datatable/__with_options.blade.php
+++ resources/views/theme/pages/Client/__datatable/__with_options.blade.php
@@ -6,7 +6,7 @@
                     <div class="col-lg-8">
 
                         <div class="col-lg-4 mb-4">
-                            <a href="#" type="button" onclick="openFilters()" class="btn btn-primary" >
+                            <a href="#" type="button" onclick="openFilters()" class="btn btn-primary">
                                 Filters
                             </a>
                             <a href="{{ route('admin:clients.create') }}" type="button" class="btn btn-info">
@@ -17,59 +17,62 @@
                 </div>
                 <table id="datatable-buttons" class="table table-bordered dt-responsive nowrap w-100">
                     <thead>
-                        <tr>
-                            {{--<th style="width: 20px;" class="align-middle">
-                                <div class="form-check font-size-16">
-                                    <input class="form-check-input" type="checkbox" id="checkAll">
-                                    <label class="form-check-label" for="checkAll"></label>
-                                </div>
-                            </th>--}}
-                            <th scope="col">Code Client</th>
-                            <th scope="col">Entreprise</th>
-                            <th scope="col">Telephone</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">ICE</th>
-                            <th scope="col">RC</th>
-                            <th scope="col">Tickets</th>
+                    <tr>
+                        {{--<th style="width: 20px;" class="align-middle">
+                            <div class="form-check font-size-16">
+                                <input class="form-check-input" type="checkbox" id="checkAll">
+                                <label class="form-check-label" for="checkAll"></label>
+                            </div>
+                        </th>--}}
+                        <th scope="col">Code Client</th>
+                        <th scope="col">Entreprise</th>
+                        <th scope="col">Telephone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">ICE</th>
+                        <th scope="col">RC</th>
+                        <th scope="col">Tickets</th>
+                        @if(auth()->user()->hasAnyRole('Admin','SuperAdmin'))
                             <th scope="col">Action</th>
-                        </tr>
+                        @endif
+                    </tr>
                     </thead>
 
                     <tbody>
 
-                        @foreach ($clients as $client)
-                            <tr>
-                                {{--<td>
-                                    <div class="form-check font-size-16">
-                                        <input class="form-check-input" type="checkbox"
-                                            id="client-{{ $client->id }}">
-                                        <label class="form-check-label" for="client-{{ $client->id }}"></label>
-                                    </div>
-                                </td>--}}
-                                <td>
-                                    <a href="{{ $client->url }}" class="text-body fw-bold">
-                                        {{ $client->code }}
-                                    </a>
-                                </td>
-                                <td>
-                                    {{ $client->entreprise }}
-                                    <p class="text-muted mb-0">{{$client->contact}}</p>
-                                </td>
-                                <td>
-                                    {{ $client->telephone }}
-                                </td>
-                                <td>
-                                    {{ $client->email }}
-                                </td>
-                                <td>
-                                    {{ $client->ice }}
-                                </td>
-                                <td>
-                                    {{ $client->rc }}
-                                </td>
-                                <td>
-                                    {{ $client->tickets_count }}
-                                </td>
+                    @foreach ($clients as $client)
+                        <tr>
+                            {{--<td>
+                                <div class="form-check font-size-16">
+                                    <input class="form-check-input" type="checkbox"
+                                        id="client-{{ $client->id }}">
+                                    <label class="form-check-label" for="client-{{ $client->id }}"></label>
+                                </div>
+                            </td>--}}
+                            <td>
+                                <a href="{{ $client->url }}" class="text-body fw-bold">
+                                    {{ $client->code }}
+                                </a>
+                            </td>
+                            <td>
+                                {{ $client->entreprise }}
+                                <p class="text-muted mb-0">{{$client->contact}}</p>
+                            </td>
+                            <td>
+                                {{ $client->telephone }}
+                            </td>
+                            <td>
+                                {{ $client->email }}
+                            </td>
+                            <td>
+                                {{ $client->ice }}
+                            </td>
+                            <td>
+                                {{ $client->rc }}
+                            </td>
+                            <td>
+                                {{ $client->tickets_count }}
+                            </td>
+                            @if(auth()->user()->hasAnyRole('Admin','SuperAdmin'))
                                 <td>
                                     <div class="d-flex gap-3">
 
@@ -77,25 +80,26 @@
                                             <i class="mdi mdi-pencil font-size-18"></i>
                                         </a>
                                         <a href="#" class="text-danger" onclick="
-                                                var result = confirm('Are you sure you want to delete this client ?');
+                                            var result = confirm('Are you sure you want to delete this client ?');
 
-                                                if(result){
-                                                    event.preventDefault();
-                                                    document.getElementById('delete-client-{{ $client->uuid }}').submit();
-                                                }">
+                                            if(result){
+                                            event.preventDefault();
+                                            document.getElementById('delete-client-{{ $client->uuid }}').submit();
+                                            }">
                                             <i class="mdi mdi-delete font-size-18"></i>
                                         </a>
                                     </div>
                                 </td>
                                 <form id="delete-client-{{ $client->uuid }}" method="post"
-                                    action="{{ route('admin:clients.delete') }}">
+                                      action="{{ route('admin:clients.delete') }}">
                                     @csrf
                                     @method('DELETE')
                                     <input type="hidden" name="clientId" value="{{ $client->uuid }}">
                                 </form>
-                            </tr>
+                            @endif
+                        </tr>
 
-                        @endforeach
+                    @endforeach
 
                     </tbody>
                 </table>
