d2271d11d06ecd157e7f5c1cbaa1e8f6333fe96a
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-25 23:15:00 +0100

    make same stuff in errors code

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout')
 
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('Service Unavailable'))
+@section('title', __('application is Under Maintenance'))
+{{--@section('code', '503')--}}
+@section('message', __('application is Under Maintenance'))
