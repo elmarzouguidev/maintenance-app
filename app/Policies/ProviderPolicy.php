335c99b1abf85570d5061e8563987b981aae7504
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-25 19:18:03 +0100

    Add ProviderPolicy n2

--- app/Policies/ProviderPolicy.php
+++ app/Policies/ProviderPolicy.php
@@ -19,7 +19,7 @@ class ProviderPolicy
      */
     public function viewAny(User $user)
     {
-        //
+        return $user->hasAnyRole('SuperAdmin', 'Admin') || $user->hasPermissionTo('providers.browse');
     }
 
     /**
@@ -31,7 +31,7 @@ class ProviderPolicy
      */
     public function view(User $user, Provider $provider)
     {
-        return $user->hasAnyRole('SuperAdmin', 'Admin') || $user->hasPermissionTo('providers.browse');
+        return $user->hasAnyRole('SuperAdmin', 'Admin') || $user->hasPermissionTo('providers.read');
     }
 
     /**
