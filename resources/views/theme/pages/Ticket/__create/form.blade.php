f564e59648db39a7f9695e91513148110ff6ab1b
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-25 16:47:35 +0100

    add errors list in create ticket form

--- resources/views/theme/pages/Ticket/__create/form.blade.php
+++ resources/views/theme/pages/Ticket/__create/form.blade.php
@@ -14,6 +14,11 @@
                         {{ session('error') }}
                     </div>
                 @endif
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        <div class="alert alert-danger">{{ $error }}</div>
+                    @endforeach
+                @endif
                 <form id="ticketForm" action="{{ route('admin:tickets.createPost') }}" method="post"
                     enctype="multipart/form-data">
                     @csrf
