b864c6090a7a932081375350c6dc6402ca986bf0
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-26 01:48:25 +0100

    add job tp backup database v2

--- config/backup.php
+++ config/backup.php
@@ -167,7 +167,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => 'abdelgha4or@gmail.com',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
