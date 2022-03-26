55fe17d91c498181007d195fdf4ec2de54fbc983
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-26 04:35:50 +0100

    secure backup route with Role middleware v2

--- routes/app-routes/backup.php
+++ routes/app-routes/backup.php
@@ -3,13 +3,11 @@
 use App\Http\Controllers\Administration\Backup\BackupController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'app'], function () {
 
-    Route::get('/', [BackupController::class, 'getFiles'])->name('index');
-    Route::post('/', [BackupController::class, 'makeBackup'])->name('make');
-    Route::delete('/', [BackupController::class, 'deleteBackup'])->name('delete');
+Route::get('/', [BackupController::class, 'getFiles'])->name('index');
+Route::post('/', [BackupController::class, 'makeBackup'])->name('make');
+Route::delete('/', [BackupController::class, 'deleteBackup'])->name('delete');
 
-    Route::post('/download/', [BackupController::class, 'downloadFile'])->name('download');
+Route::post('/download/', [BackupController::class, 'downloadFile'])->name('download');
 
-    Route::get('/db', [BackupController::class, 'backupOnlyDb'])->name('db');
-});
+Route::get('/db', [BackupController::class, 'backupOnlyDb'])->name('db');
