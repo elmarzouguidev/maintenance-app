11b1099c2001f4eebf389b90b0cc094f22df4398
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-04-20 18:38:29 +0000

    add button to add images for tiket

--- resources/views/theme/pages/Ticket/__edit/__ticket_actions.blade.php
+++ resources/views/theme/pages/Ticket/__edit/__ticket_actions.blade.php
@@ -9,10 +9,15 @@
                        class="btn btn-primary waves-effect waves-light w-sm">
                         <i class="mdi mdi-file-pdf d-block font-size-16"></i> Télécharger le Rapport
                     </a>
-
+                    <a 
+                       href="{{ $ticket->media_url }}"
+                       class="btn btn-info waves-effect waves-light w-sm">
+                        <i class="mdi mdi-file-image  d-block font-size-16"></i>  Ajouter des photos
+                    </a>
                     <button type="button" class="btn btn-danger waves-effect waves-light w-sm" id="deleteTicket">
                         <i class="mdi mdi-trash-can d-block font-size-16"></i> Supprimer
                     </button>
+   
 
                     <form id="delete-ticket-single-{{ $ticket->uuid }}" method="post"
                           action="{{ route('admin:tickets.delete') }}">
