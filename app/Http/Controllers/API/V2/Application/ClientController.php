176c1672cb828cbb4401e9484b0d244d9ba7155b
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-02 19:15:17 +0000

    add V2 Api

--- app/Http/Controllers/API/V2/Application/ClientController.php
+++ app/Http/Controllers/API/V2/Application/ClientController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\API\V2\Application;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Http\Resources\Application\Client\ClientResource;
+use App\Models\Client;
 class ClientController extends Controller
 {
-    //
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(
+            [
+                'payload' =>  ClientResource::collection(Client::all()),
+                '_response' => ['msg' => 'successfully Brand']
+            ],
+            200
+        );
+    }
 }
