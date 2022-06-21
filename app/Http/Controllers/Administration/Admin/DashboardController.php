a0747c45bc5e8556145456c95555fc4704c8be78
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-06-21 22:00:26 +0100

    add laraveldaily charts package to the app v3

--- app/Http/Controllers/Administration/Admin/DashboardController.php
+++ app/Http/Controllers/Administration/Admin/DashboardController.php
@@ -130,7 +130,7 @@ class DashboardController extends Controller
         $companies = Company::select(['id', 'uuid', 'name'])->get();
 
         $chart_options = [
-            'chart_title' => 'Chiffre d\'affaire',
+            'chart_title' => "Chiffre d'affaire",
             'report_type' => 'group_by_date',
             'model' => 'App\Models\Finance\Bill',
             'group_by_field' => 'created_at',
--- resources/views/theme/pages/Home/sections/section_a_chart.blade.php
+++ resources/views/theme/pages/Home/sections/section_a_chart.blade.php
@@ -10,3 +10,16 @@
         {{--<div id="stacked-column-chart" class="apex-charts" dir="ltr"></div>--}}
     </div>
 </div> 
+
+<div class="card">
+    <div class="card-body">
+        <div class="d-sm-flex flex-wrap">
+
+            <h4 class="card-title mb-4">{{ $chart2->options['chart_title'] }}</h4>
+
+            {!! $chart2->renderHtml() !!}
+        </div>
+        
+        {{--<div id="stacked-column-chart" class="apex-charts" dir="ltr"></div>--}}
+    </div>
+</div> 
