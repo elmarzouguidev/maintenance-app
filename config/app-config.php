91b6e9f31e90c4d422a4d6d3e857ce30ea2a0ada
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-06-26 00:34:42 +0100

    push chiffre d'affaire v5

--- config/app-config.php
+++ config/app-config.php
@@ -32,13 +32,13 @@ return [
     'invoices' => [
         'prefix' => 'FACTURE-',
         'start_from' => 800,
-        'due_date_after' => 10
+        'due_date_after' => 60
     ],
 
     'estimates' => [
         'prefix' => 'DEVIS-',
         'start_from' => 1501,
-        'due_date_after' => 10,
+        'due_date_after' => 15,
         'default_condition' => "Délai 2 semaines après la réception de bon de commande"
     ],
 
