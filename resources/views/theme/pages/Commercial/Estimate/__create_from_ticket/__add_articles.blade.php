3baf87b86641fdc9fe48cd8bb4dbe54ef90cd3c8
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-10-19 09:17:01 +0100

    fix error when create Estimate From Ticket

--- resources/views/theme/pages/Commercial/Estimate/__create_from_ticket/__add_articles.blade.php
+++ resources/views/theme/pages/Commercial/Estimate/__create_from_ticket/__add_articles.blade.php
@@ -1,6 +1,6 @@
 <div data-repeater-list="articles">
     <div data-repeater-item class="row">
-        <div class="mb-3 col-lg-6">
+        <div class="mb-3 col-lg-5">
             <label for="designation">{{__('estimate.form.article_designation')}} *</label>
             <textarea name="designation" id="designation" rows="5"
                 class="form-control @error('articles.*.designation') is-invalid @enderror" required></textarea>
@@ -11,20 +11,9 @@
             @enderror
         </div>
 
-        {{--<div class="mb-3 col-lg-2">
-            <label for="description">{{__('estimate.form.article_description')}}</label>
-            <textarea name="description" id="description" rows="5"
-                class="form-control @error('articles.*.description') is-invalid @enderror"></textarea>
-            @error('description')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>--}}
-
         <div class="mb-3 col-lg-1">
             <label for="quantity">{{__('estimate.form.article_qte')}} *</label>
-            <input type="text" name="quantity" id="quantity" min="1"
+            <input type="text" name="quantity" id="quantity" 
                 class="form-control @error('articles.*.quantity') is-invalid @enderror" required />
             @error('quantity')
                 <span class="invalid-feedback" role="alert">
@@ -44,7 +33,16 @@
                 </span>
             @enderror
         </div>
-
+        <div class="mb-3 col-lg-1">
+            <label for="remise">{{__('Remise %')}} </label>
+            <input type="number" name="remise" id="remise" value="0" min="0"
+                class="form-control @error('articles.*.remise') is-invalid @enderror"  />
+            @error('remise')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
         <div class="mb-3 col-lg-2">
             <label for="montant_ht">{{__('estimate.form.article_total_ht')}}</label>
             <input type="text" name="montant_ht" id="montant_ht"
