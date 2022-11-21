5df3ee1dc2f4db10835ad17abe2307b6ec30bc4e
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-11-21 12:21:46 +0100

    add update article to Invoice

---
+++ app/Http/Livewire/Commercial/Invoice/Edit/EditArticle.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Livewire\Commercial\Invoice\Edit;
+
+use Livewire\Component;
+use App\Http\Requests\Commercial\Invoice\ArticleUpdateFormRequest;
+use App\Models\Finance\Article;
+use App\Models\Finance\Invoice;
+use App\Services\Commercial\Taxes\TVACalulator;
+use App\Services\Commercial\Remise\RemiseCalculator;
+
+class EditArticle extends Component
+{
+
+    use TVACalulator;
+    use RemiseCalculator;
+
+    public Article $article;
+    public Invoice $invoice;
+
+    public $designation;
+    public $quantity;
+    public $prix_unitaire;
+    public $remise;
+    public $montant_ht;
+
+    public function render()
+    {
+        return view('theme.livewire.commercial.invoice.edit.edit-article');
+    }
+
+    public function mount()
+    {
+        $this->designation = str_replace('<br />', '', $this->article->designation);
+        $this->quantity = $this->article->quantity;
+        $this->prix_unitaire = $this->article->prix_unitaire;
+        $this->remise = $this->article->remise;
+        $this->montant_ht = $this->article->montant_ht;
+    }
+
+    public function rules()
+    {
+        return (new ArticleUpdateFormRequest())->rules();
+    }
+
+    public function updateArticle()
+    {
+
+        $invoice = $this->invoice;
+
+        $article = $this->article;
+
+        $oldArticlePrice = $article->montant_ht;
+
+        if ($article && $invoice) {
+
+            $itemPrice = $this->prix_unitaire * $this->quantity;
+            $finalePrice = $this->caluculateRemise($itemPrice, $this->remise ?? 0);
+            $tauxRemise = $this->calculateOnlyRemise($itemPrice, $this->remise ?? 0);
+            $article->update([
+                'designation' => $this->designation,
+                'quantity' => $this->quantity,
+                'prix_unitaire' => $this->prix_unitaire,
+                'montant_ht' => $finalePrice,
+                'remise' => $this->remise ?? 0,
+                'taux_remise' => $tauxRemise ?? 0
+            ]);
+        }
+
+        $totalPrice = ($invoice->price_ht - $oldArticlePrice) + $article->montant_ht;
+        $invoice->price_ht = $totalPrice;
+        $invoice->price_total = $this->caluculateTva($totalPrice);
+        $invoice->price_tva = $this->calculateOnlyTva($totalPrice);
+
+        $invoice->save();
+
+        $invoice->histories()->create([
+            'user_id' => auth()->id(),
+            'user' => auth()->user()->full_name,
+            'detail' => 'a modifier un article depuis le DEVIS ',
+            'action' => 'update'
+        ]);
+
+        return redirect($invoice->edit_url)->with('success', "L'article a été modifier avec success");
+    }
+}
