15618dd5c7ee154411fe035afe3e45ab65e09d22
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-01-20 13:43:05 +0100

    Hide Send Invoice From Estimate table

--- resources/views/theme/pages/Commercial/Estimate/__datatable/__estimates_table.blade.php
+++ resources/views/theme/pages/Commercial/Estimate/__datatable/__estimates_table.blade.php
@@ -2,6 +2,5 @@
 
 @include('theme.pages.Commercial.Estimate.__datatable.__with_options')
 
-@each('theme.pages.Commercial.Estimate.__datatable.__send_estimate',$estimates ,'estimate' )
-
-@each('theme.pages.Commercial.Estimate.__edit.__print_document',$estimates,'estimate')
+@each('theme.pages.Commercial.Estimate.__edit.__print_document', $estimates, 'estimate')
+{{-- @each('theme.pages.Commercial.Estimate.__datatable.__send_estimate',$estimates ,'estimate' ) --}}
