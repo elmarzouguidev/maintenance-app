3feeb2de2cb349162d48a218d157e07bb46717fa
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2023-02-28 14:45:00 +0100

    hide TTC in

---
+++ app/Http/Controllers/Administration/Statistique/StatistiqueController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Administration\Statistique;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StatistiqueController extends Controller
+{
+    //
+}
--- resources/views/theme/pages/Home/sections/section_a_orders.blade.php
+++ resources/views/theme/pages/Home/sections/section_a_orders.blade.php
@@ -1,29 +1,32 @@
 <div class="col-xl-12">
     <div class="row">
-        <div class="col-md-4">
-            <div class="card mini-stats-wid">
-                <div class="card-body">
-                    <div class="d-flex">
-                        <div class="flex-grow-1">
-                            <p class="text-muted fw-medium">Chiffre d'affaires (T.T.C)</p>
-                            <h4 class="mb-0">{{ number_format($chiffreAffTTC, 2) }}</h4>
-                        </div>
+        @hasanyrole('SuperAdmin|Admin')
+            <div class="col-md-4">
+                <div class="card mini-stats-wid">
+                    <div class="card-body">
+                        <div class="d-flex">
 
-                        <div class="flex-shrink-0 align-self-center">
-                            <div class="mini-stat-icon avatar-sm rounded-circle bg-primary">
+                            <div class="flex-grow-1">
+                                <p class="text-muted fw-medium">Chiffre d'affaires (T.T.C)</p>
+                                <h4 class="mb-0">{{ number_format($chiffreAffTTC, 2) }}</h4>
+                            </div>
 
-                                <a href="{{ route('admin:tickets.list') }}">
-                                    <span class="avatar-title">
-                                        <i class="bx bx-copy-alt font-size-24"></i>
-                                    </span>
-                                </a>
+                            <div class="flex-shrink-0 align-self-center">
+                                <div class="mini-stat-icon avatar-sm rounded-circle bg-primary">
 
+                                    <a href="{{ route('commercial:invoices.index') }}">
+                                        <span class="avatar-title">
+                                            <i class="bx bx-copy-alt font-size-24"></i>
+                                        </span>
+                                    </a>
+
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endhasanyrole
         <div class="col-md-4">
             <div class="card mini-stats-wid">
                 <div class="card-body">
@@ -65,5 +68,4 @@
             </div>
         </div>
     </div>
-
 </div>
