464f76564d7c9ae0fc84f767077c667078d4e0da
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2023-07-12 13:03:21 +0100

    update laguages é

--- public/js/pages/datatables.init.js
+++ public/js/pages/datatables.init.js
@@ -10,9 +10,23 @@ $(document).ready(function () {
         autoWidth: true,
         pageLength: 60,
         columnDefs: [
-            { "width": "10",},
-            { "width": "10%",}
-          ]
+            { "width": "10", },
+            { "width": "10%", }
+        ],
+        language: {
+            "lengthMenu": "Afficher _MENU_ éléments par page",
+            "zeroRecords": "Aucune donnée disponible dans le tableau",
+            "info": "Affichage de l'élément _PAGE_ sur _PAGES_",
+            "infoEmpty": "Aucune donnée disponible",
+            "infoFiltered": "(filtered from _MAX_ total records)",
+            "paginate": {
+                "first": "First",
+                "last": "Last",
+                "next": "Suivant",
+                "previous": "Précédent"
+            },
+            "search": "Chercher :",
+        },
     });
 
     table.buttons().container()
