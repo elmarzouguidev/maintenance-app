976955e77f62a37f4d1b4e4f462c6e5caad9393d
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-11-28 13:09:30 +0100

    update 503 page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,54 @@
-@extends('errors::illustrated-layout')
+<!doctype html>
+<html lang="en">
 
-@section('title', __('application is Under Maintenance'))
-{{--@section('code', '503')--}}
-@section('message', __('application is Under Maintenance'))
+<head>
+    <meta charset="utf-8" />
+
+    <title>Casamaintenance APP INDISPONIBLE</title>
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="app_creator" name="Elmarzougui Abdelghafour" />
+    <meta content="app_version" name="1.1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+    <link rel="shortcut icon" href="{{ asset('images/logo-app-2.png') }}">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="account-pages my-5 pt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center mb-5">
+
+                        <h1 class="display-2 fw-medium">5<i class="bx bx-buoy bx-spin text-primary display-3"></i>3</h1>
+                        <h4 class="">
+                            Veuillez nous excuser pour la gêne occasionnée.<br> <b>Casamaintenance APP</b> sera à nouveau disponible sous peu, revenez bientôt !
+                        </h4>
+                        <div class="mt-5 text-center">
+                            <a class="btn btn-primary waves-effect waves-light" href="https://casamaintenance.ma">casamaintenance.ma</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-xl-6">
+                    <div>
+                        <img src="{{ asset('assets/images/error-img.png') }}" alt="" class="img-fluid">
+                    </div>
+                 
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+
+</html>
