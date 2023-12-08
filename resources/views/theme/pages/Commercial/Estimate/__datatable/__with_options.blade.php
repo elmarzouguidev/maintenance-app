19f0b3657ea226f96f1e5c37e5ab3ddac0a88d47
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-12-08 15:05:45 +0100

    update

--- resources/views/theme/pages/Commercial/Estimate/__datatable/__with_options.blade.php
+++ resources/views/theme/pages/Commercial/Estimate/__datatable/__with_options.blade.php
@@ -68,7 +68,7 @@
                                             @else
                                                 @foreach ($estimate->tickets as $tickett)
                                                     <span class="badge rounded-pill bg-primary"> {{ $tickett->code }}
-                                                    </span> {{ $loop->iteration == 4 ? '<br>' : '' }}
+                                                    </span> {!! $loop->iteration == 4 ? '<br>' : '' !!}
                                                     {{ $loop->last ? '' : ' ,' }}
                                                 @endforeach
                                             @endif
