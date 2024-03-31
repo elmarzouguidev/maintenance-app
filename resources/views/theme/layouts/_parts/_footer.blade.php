563e69f97c641b271c4407c070493391be7284dc
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-03-31 14:41:16 +0000

    update packages : 31-03-2024

--- resources/views/theme/layouts/_parts/_footer.blade.php
+++ resources/views/theme/layouts/_parts/_footer.blade.php
@@ -2,10 +2,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © ERP CASA MAINTENANCE.
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> © ERP CASA MAINTENANCE.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
+                    developed by
                     <a href="https://wedoapp.ma/" target="_blank" class="text-muted" style="color: blue !important">
 
                         WEDOAPP
