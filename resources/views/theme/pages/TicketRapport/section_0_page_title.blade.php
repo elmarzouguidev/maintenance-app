c1992d940ebeaa195d5c87217b1c6740a3f3f895
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2025-05-03 17:00:54 +0100

    add Report Controller to view and laod PDFs v-4

--- resources/views/theme/pages/TicketRapport/section_0_page_title.blade.php
+++ resources/views/theme/pages/TicketRapport/section_0_page_title.blade.php
@@ -1,14 +1,15 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-            <h4 class="mb-sm-0 font-size-18">{{ __('navbar.clients') }}</h4>
+            <h4 class="mb-sm-0 font-size-18">LES RAPPORTS TECHNIQUES</h4>
 
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item"><a
-                            href="{{ route('admin:clients.index') }}">{{ __('navbar.clients') }}</a></li>
-                    <li class="breadcrumb-item"><a
-                            href="{{ route('admin:clients.create') }}">{{ __('navbar.clients_add') }}</a></li>
+                            href="{{ route('admin:rapports.index') }}">LES RAPPORTS TECHNIQUES
+                        </a>
+                    </li>
+   
                 </ol>
             </div>
 
