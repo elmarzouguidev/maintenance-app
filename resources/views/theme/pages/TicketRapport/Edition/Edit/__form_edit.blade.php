d400d34f08cc131da380f3bf6678f19aa22cd42e
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2025-05-07 16:18:20 +0100

    update packages v-14

--- resources/views/theme/pages/TicketRapport/Edition/Edit/__form_edit.blade.php
+++ resources/views/theme/pages/TicketRapport/Edition/Edit/__form_edit.blade.php
@@ -13,7 +13,23 @@
                     <div class="row">
                         <div class="col-lg-6">
 
-                            @include('theme.pages.Ticket.__edit.__info')
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="mb-4">
+                                        <label class="form-label">Ticket *</label>
+                                        <input id="article" name="article" type="text"
+                                               class="form-control @error('article') is-invalid @enderror"
+                                               value="{{$report->ticket?->article}}">
+                                        @error('article')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                            
+                                    </div>
+                                </div>
+                            
+                            </div>
 
                             <div class="docs-options">
                                 <label class="form-label">Numéro de Ticket</label>
